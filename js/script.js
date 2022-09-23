142e5a583ad4f1d73617715af2181d5302653253
Francesco Natali <34441930+francesconatali@users.noreply.github.com>
2022-09-23 17:20:26 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+/* 
+  Application logic to handle the processing of submitted text.
+*/
+const POST_COMMENT_BTN = document.getElementById('post');
+const COMMENT_TEXT = document.getElementById('comment');
+const COMMENTS_LIST = document.getElementById('commentsList');
+// CSS styling class to indicate comment is being processed when
+// posting to provide visual feedback to users.
+const PROCESSING_CLASS = 'processing';
+// CSS styling class to indicate comment is toxic.
+const TOXIC_CLASS = 'toxic';
+// Anything equal or above is considered toxic.
+const TOXIC_THRESHOLD = 75;
+
+POST_COMMENT_BTN.addEventListener('click', handleCommentPost);
+
+/*
+  Function to handle the processing of submitted text.
+*/
+function handleCommentPost() {
+  // Only continue if there's no other ongoing processing.
+  if (! POST_COMMENT_BTN.classList.contains(PROCESSING_CLASS)) {
+    // Get the text to validate.
+    let currentComment = COMMENT_TEXT.innerText.trim();
+    // Double-check there's something to validate
+    if (currentComment.length === 0) {
+      // Reset the comment text box.
+      COMMENT_TEXT.innerText = '';
+      // Focus back to the comment text box.
+      COMMENT_TEXT.focus();
+    } else {
+      // Visually mark the comment as being processed.
+      POST_COMMENT_BTN.classList.add(PROCESSING_CLASS);
+      COMMENT_TEXT.classList.add(PROCESSING_CLASS);
+     
+      // Create a list item DOM element in memory.
+      let div = document.createElement('div');
+      div.classList.add('relative', 'col-span-full', 'rounded-2xl', 'overflow-hidden', 'border', 'border-gray-80', 'border-opacity-20', 'shadow-2xl', 'mt-8', 'p-4', 'md:text-2xl', 'text-gray-80');
+      let p = document.createElement('p');
+      p.innerText = currentComment.split(' ').slice(0, 20).join(' ');
+
+      div.appendChild(p);
+      COMMENTS_LIST.prepend(div);
+
+      // Reset comment text.
+      COMMENT_TEXT.innerText = '';
+      
+      // loadAndPredict (see model.js) is asynchronous so I use the then 
+      // keyword to await a result before continuing.
+      loadAndPredict(currentComment).then(function(toxiticyLevel) {
+        // Reset class styles ready for the next comment.
+        POST_COMMENT_BTN.classList.remove(PROCESSING_CLASS);
+        COMMENT_TEXT.classList.remove(PROCESSING_CLASS);
+        // Act on the result
+        if (toxiticyLevel >= TOXIC_THRESHOLD) {
+          // Visually mark the text as toxic.
+          div.classList.add(TOXIC_CLASS);
+        } else {
+          // Non-toxic text, nothing to do.
+        }
+      });
+    }
+  }
+}
---
+++ model/model.js
@@ -0,0 +1,84 @@
+/* 
+  This file contains the code for the Machine Learning model.
+
+  !! There should be no reason for you to change anything here, 
+  apart from the following two files' name/path if you need to update them.
+
+  Note: this file has to be loaded using the type="module" attribute,
+  eg. <script type="module" src="..."></script>
+*/
+import * as DICTIONARY from './dictionary.js';
+const MODEL_JSON_URL = './model/model.json';
+// The number of input elements the ML Model is expecting,
+// 21 because we have 20 words plus the START token.
+const ENCODING_LENGTH = 21;
+
+var model = undefined;
+
+// As JavaScript modules by default don't interact with non module script files
+// I need to export the loadAndPredict function so that it can be called from outside (file script.js)
+window.loadAndPredict = async function loadAndPredict(inputText) {
+  // Load the model.json and binary files. Note this is 
+  // an asynchronous operation so I use the await keyword
+  if (model === undefined) {
+    model = await tf.loadLayersModel(MODEL_JSON_URL);
+  }
+  
+  // Convert sentence to lower case which ML Model expects
+  // Strip all characters that are not alphanumeric or spaces
+  // Then split on spaces to create a word array.
+  let lowercaseSentenceArray = inputText.toLowerCase().replace(/[^\w\s]/g, ' ').split(' ');
+  
+  // First 20 words only
+  lowercaseSentenceArray = lowercaseSentenceArray.slice(0, 20);
+  
+  // Once model has loaded, I can now call model.predict and pass to it
+  // an input in the form of a Tensor and then store the result.
+  let results = model.predict(tokenize(lowercaseSentenceArray));
+  
+  // Print the result to the console for inspection.
+  // results.print();
+
+  // Extract the values from the Tensor and store in a JavaScript array.
+  let dataArray = results.dataSync();
+
+  // dataArray[1] contains the probability of the text being toxic
+  // and it can be converted to a percentage by multiplying by 100
+  // to make it easier to read and handle.
+  let toxiticyLevel = dataArray[1] * 100;
+
+  // Print the Toxiticy level for inspection.
+  // console.log('Toxiticy level: ' + toxiticyLevel);
+
+  return toxiticyLevel;
+}
+
+/** 
+ * Function that takes an array of words, converts words to tokens,
+ * and then returns a Tensor representation of the tokenization that
+ * can be used as input to the machine learning model.
+ */
+function tokenize(wordArray) {
+  // Always start with the START token.
+  let returnArray = [DICTIONARY.START];
+  
+  // Loop through the words in the sentence you want to encode.
+  // If word is found in dictionary, add that number else
+  // you add the UNKNOWN token.
+  for (var i = 0; i < wordArray.length; i++) {
+    let encoding = DICTIONARY.LOOKUP[wordArray[i]];
+    returnArray.push(encoding === undefined ? DICTIONARY.UNKNOWN : encoding);
+  }
+  
+  // Finally if the number of words was < the minimum encoding length
+  // fill the rest with PAD tokens.
+  while (returnArray.length < ENCODING_LENGTH) {
+    returnArray.push(DICTIONARY.PAD);
+  }
+  
+  // Log the result for inspection.
+  // console.log([returnArray]);
+  
+  // Convert to a TensorFlow Tensor 2D and return that.
+  return tf.tensor2d([returnArray]);
+}
